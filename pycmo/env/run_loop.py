7fae32c051dac6e4b32bcc1d5f1fd94c05f14f8b
duyminh1998 <minhhua12345@gmail.com>
2021-06-01 15:20:35 -0700

    implement step function

--- pycmo/env/run_loop.py
+++ pycmo/env/run_loop.py
@@ -3,12 +3,15 @@
 from pycmo.lib.client import Client
 from pycmo.lib.server import Server
 import threading
+import time
 
-client = Client()
 server = Server("C:\\Program Files (x86)\\Command Professional Edition\\Scenarios\\Standalone Scenarios\\Battle of Chumonchin Chan, 1950.scen")
-
 x = threading.Thread(target=server.start_game)
 x.start()
 
+time.sleep(15)
+
+client = Client()
 for i in range(5):
-    client.get_raw_data("C:\\Users\\Public\\Desktop\\scenario" + str(i) + ".xml")
+    client.get_raw_data("C:\\\\Users\\\\Public\\\\Desktop\\\\scenario" + str(i) + ".xml")
+    client.step("01", "00", "00")
--- pycmo/lib/client.py
+++ pycmo/lib/client.py
@@ -11,8 +11,8 @@ class Client():
         self.s.connect((self.host, self.port))
 
     def get_raw_data(self, destination):
-        data = "file = io.open({}, \"w\") \n".format(destination)
-        data += "io.output(file) \n theXML = ScenEdit_ExportScenarioToXML() \n io.write(theXML) \n io.close(file)"
+        data = "--script \nfile = io.open('{}', 'w') \n".format(destination)
+        data += "io.output(file) \ntheXML = ScenEdit_ExportScenarioToXML()\nio.write(theXML) \nio.close(file)"
         self.send_action(data)
 
     def send_action(self, data):
@@ -21,8 +21,8 @@ class Client():
         received = str(data, "utf-8")
         print(received)
 
-    def step(self):
-        pass
+    def step(self, h, m, s):
+        self.send_action("--script VP_RunForTimeAndHalt({Time='" + str(h) + ":" + str(m) + ":" + str(s) + "'})")
 
     def restart(self):
         try:
@@ -38,4 +38,6 @@ class Client():
 
 if __name__ == "__main__":
     blue = Client("United Nations")
-    blue.send_action(data = "--script \n VP_RunSimulation() \n local unit = VP_GetUnit({side = 'United Nations', name = 'HMS Black Swan'}) \n file = io.open('C:\\\\Users\\\\Public\\\\Desktop\\\\test.txt', 'w') \n io.output(file) \n io.write(unit.name) \n io.close(file) \n VP_PauseSimulation()")
+    for i in range(5):
+        blue.get_raw_data("C:\\\\Users\\\\Public\\\\Desktop\\\\scenario" + str(i) + ".xml")
+        blue.step("01", "00", "00")
