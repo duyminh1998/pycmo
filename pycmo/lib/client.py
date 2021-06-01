d53d55fbc7ce075da98664f6f4fed341e7363bf0
duyminh1998 <minhhua12345@gmail.com>
2021-06-01 11:45:15 -0700

    update client and server files, add ipynb to gitignore

--- pycmo/lib/client.py
+++ pycmo/lib/client.py
@@ -1,12 +1,33 @@
+"""Protocol library to communicate with a Command server."""
+
+import os
 import socket
 
-host = "localhost"
-port = 7777                   # The same port as used by the server
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((host, port))
-data = "--script \n VP_RunSimulation() \n local unit = VP_GetUnit({side = 'United Nations', name = 'HMS Black Swan'}) \n file = io.open('C:\\\\Users\\\\Public\\\\Desktop\\\\test.txt', 'w') \n io.output(file) \n io.write(unit.name) \n io.close(file) \n VP_PauseSimulation()"
-s.sendall(data.encode(encoding='UTF-8'))
-data = s.recv(1024)
-received = str(data, "utf-8")
-s.close()
-print(received)
+class Client():
+    def __init__(self, side):
+        self.host = "localhost"
+        self.port = 7777                   # The same port as used by the server
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def get_observation(self):
+        pass
+
+    def send_action(self, data):
+        self.s.connect((self.host, self.port))
+        self.s.sendall(data.encode(encoding='UTF-8'))
+        data = self.s.recv(1024)
+        received = str(data, "utf-8")
+        self.s.close()
+        print(received)
+
+    def step(self):
+        pass
+
+    def restart(self):
+        pass
+
+    def end_game(self):
+        pass
+
+blue = Client("United Nations")
+blue.send_action(data = "--script \n VP_RunSimulation() \n local unit = VP_GetUnit({side = 'United Nations', name = 'HMS Black Swan'}) \n file = io.open('C:\\\\Users\\\\Public\\\\Desktop\\\\test.txt', 'w') \n io.output(file) \n io.write(unit.name) \n io.close(file) \n VP_PauseSimulation()")
--- pycmo/lib/server.py
+++ pycmo/lib/server.py
@@ -1,10 +1,9 @@
-"""Protocol library to communicate with Command."""
+"""Protocol library to host a Command headless server."""
 
-import socket
 import os
 import subprocess
 
-class CommandProtocol():
+class Server():
     def __init__(self, side):
         self.side = side
 
@@ -15,7 +14,6 @@ class CommandProtocol():
             command = "CommandCLI.exe -mode I -scenfile \"{}\"".format(scen_path)
             print(command)
             subprocess.call(command, shell=True, cwd = prog_path)
-            
         except:
             pass
 
@@ -34,5 +32,5 @@ class CommandProtocol():
     def end_game(self):
         pass
 
-blue = CommandProtocol("United Nations")
+blue = Server("United Nations")
 blue.start_game("C:\\Program Files (x86)\\Command Professional Edition\\Scenarios\\Standalone Scenarios\\Battle of Chumonchin Chan, 1950.scen")
