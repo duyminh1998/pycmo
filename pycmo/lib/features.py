d2558dea28737a61d8b4fab2eb62e9f47ea958a4
duyminh1998 <minhhua12345@gmail.com>
2021-06-01 16:19:35 -0700

    add extract observation from raw data and config json

--- pycmo/lib/features.py
+++ pycmo/lib/features.py
@@ -1,1 +1,15 @@
-"""Export Command observations into arrays."""
+"""Export Command raw data into numpy arrays."""
+
+import xml.etree.ElementTree as ET
+
+def extract_observations(xml):
+    tree = ET.parse(xml)
+    root = tree.getroot()
+    print("Time:", root[12].text)
+    print("Duration:", root[15].text)
+    print(root[19][1][1].text)
+    for child in root[19]:
+        print(child[1].text)
+
+if __name__ == '__main__':
+    extract_observations("C:\\Users\\AFSOC A8XW ORSA\\Documents\\Python Proj\\AI\\pycmo\\raw\\scen.xml")
