0e02187f8e30cf4040ccebc2811fd181549ce609
duyminh1998 <minhhua12345@gmail.com>
2021-08-10 13:36:50 -0700

    delete raw_features.py

--- pycmo/lib/features.py
+++ pycmo/lib/features.py
@@ -8,17 +8,17 @@ import collections
 GameInfo = collections.namedtuple("GameInfo", ["TimelineID", "Time", "ScenarioName", "ZeroHour", "StartTime", "Duration", "Sides"])
 # Side/Player info
 # SideInfo = collections.namedtuple("SideInfo", ['ID', 'Name', 'Postures', 'ReferencePoints', 'Doctrine', 'TotalScore', 'Missions', 'Units'])
-SideInfo = collections.namedtuple("SideInfo", ['ID', 'Name', 'Postures', 'Doctrine', 'TotalScore', 'Units'])
+SideInfo = collections.namedtuple("SideInfo", ['ID', 'Name', 'TotalScore'])
 # Mission Info
 # MsnInfo = collections.namedtuple("MsnInfo", [])
 # Unit info
-UnitInfo = collections.namedtuple("UnitInfo", ['ID', 'Name', 'CH', 'CS', 'Lon', 'Lat', 'LonLR', 'LatLR', 'Side', 'DBID', 'DH', 'DS', 'DT', 'DTN', 
+""" UnitInfo = collections.namedtuple("UnitInfo", ['ID', 'Name', 'CH', 'CS', 'Lon', 'Lat', 'LonLR', 'LatLR', 'Side', 'DBID', 'DH', 'DS', 'DT', 'DTN', 
 'ThrottleSetting', 'Sensors', 'Comms', 'Mounts', 'Magazines', 'Status', 'FuelState', 'WeaponState', 'SBR', 'SBED', 'SBEO', 'FSBR', 'SBR_Altitude', 
 'SBR_Altitude_TF', 'SBR_TF', 'SBR_ThrottleSetting', 'SBED_Altitude', 'SBED_Altitude_TF', 'SBED_TF', 'SBED_ThrottleSetting', 'SBEO_Altitude', 'SBEO_Altitude_TF', 
 'SBEO_TF', 'SBEO_ThrottleSetting', 'AMP_OC', 'AMP_OC_DAO', 'AMP_OC_Speed', 'DamagePts', 'OldDamagePercent', 'Doctrine', 'CIC', 'Navigator', 'AI', 'Kinematics', 
-'Sensory', 'Weaponry', 'CommStuff', 'Damage', 'ActiveUnit_AirOps', 'ActiveUnit_DockingOps'])
+'Sensory', 'Weaponry', 'CommStuff', 'Damage', 'ActiveUnit_AirOps', 'ActiveUnit_DockingOps']) """
 # Contacts info
-ContactInfo = collections.namedtuple("ContactInfo", ["Name"])
+# ContactInfo = collections.namedtuple("ContactInfo", ["Name"])
 
 def features_from_game_info(xml, side):
     """Construct a Features object using data extracted from game info."""
@@ -29,19 +29,20 @@ class Features(object):
     """Render feature layers from Command scenario XML into named tuples."""
     
     def __init__(self, xml, player_side):
-            tree = ET.parse(xml) # This variable contains the XML tree
-            root = tree.getroot() # This is the root of the XML tree
-            xmlstr = ET.tostring(root)
-            self.scen_dic = xmltodict.parse(xmlstr) # our scenario xml is now in 'dic'
-            self.meta = self.get_meta() # Return the scenario-level information of the scenario
-            self.side_info = self.get_side_prop(self.meta.Sides.index(player_side))
+        tree = ET.parse(xml) # This variable contains the XML tree
+        root = tree.getroot() # This is the root of the XML tree
+        xmlstr = ET.tostring(root)
+        self.scen_dic = xmltodict.parse(xmlstr) # our scenario xml is now in 'dic'
+        self.meta = self.get_meta() # Return the scenario-level information of the scenario
+        self.units = self.get_side_units(player_side)
+        self.side_info = self.get_side_properties(self.get_sides().index(player_side))
 
     def transform_obs(self, obs):
         """Render some SC2 observations into something an agent can handle."""
         pass
 
     # XML Data Extraction Methods
-    def get_meta(self):
+    def get_meta(self) -> GameInfo:
         """Get meta data (top-level scenario data)"""
         return GameInfo(self.scen_dic['Scenario']["TimelineID"],
                         self.scen_dic['Scenario']["Time"],
@@ -55,56 +56,31 @@ class Features(object):
         """Get the number and names of all sides in a scenario"""
         return [self.scen_dic['Scenario']['Sides']['Side'][i]['Name'] for i in range(len(self.scen_dic['Scenario']['Sides']['Side']))]
 
-    def get_side_prop(self, side_id = 0):
-        """Get the properties of a side"""
-        side = self.scen_dic['Scenario']['Sides']['Side'][side_id]
-        return SideInfo(side['ID'], side['Name'], side['Postures'], "side['Doctrine']",
-                        side['TotalScore'], "side['Units']")
+    def get_side_properties(self, side_id = 0) -> SideInfo:
+        """Get the properties (score, name, etc.) of a side"""
+        return SideInfo(self.scen_dic['Scenario']['Sides']['Side'][side_id]['ID'],
+                        self.scen_dic['Scenario']['Sides']['Side'][side_id]['Name'],
+                        self.scen_dic['Scenario']['Sides']['Side'][side_id]['TotalScore'])
 
-    def get_side_units(self, side_id_str):
+    def get_side_units(self, side_id_str=None):
         """Get all the units of a side"""
+        if not side_id_str:
+            return
         ar = {}
-        ar[" "] = " "
-        if side_id_str == " ":
-            return ar
-        for i in range(len(self.root[21])):
-            for match in self.root[21][i].iterfind("Side"):
-                if match.text == side_id_str:
-                    if self.root[21][i][1].text not in ar.keys():
-                        ar[self.root[21][i][1].text] = i
-                    else:
-                        j = 1
-                        new_str = self.root[21][i][1].text + " #" + str(j)
-                        while new_str in ar.keys():
-                            j += 1
-                            new_str = self.root[21][i][1].text + " #" + str(j)
-                        ar[new_str] = i
-                    break
-        return collections.OrderedDict(sorted(ar.items()))
-
-    def get_side_unit_prop(self, unit_id_str, side_units_ar = []):
-        """Get the properties of a unit of a side"""
-        if len(side_units_ar) < 1:
-            side_units_ar = self.side_units_ar
-        ar = {}
-        if unit_id_str == " ":
-            return ar
-        for i in range(len(self.root[21][side_units_ar[unit_id_str]])):
-            try:
-                ar[self.root[21][side_units_ar[unit_id_str]][i].tag + ": " + self.root[21][side_units_ar[unit_id_str]][i].text] = i
-            except TypeError:
-                ar[self.root[21][side_units_ar[unit_id_str]][i].tag + ": "] = i
+        ar['Aircraft'] = []
+        ar['Facility'] = []
+        ar['Ship'] = []
+        ar['Group'] = []
+        ar['Submarine'] = []
+        for key in self.scen_dic["Scenario"]["ActiveUnits"].keys():
+            for i in range(len(self.scen_dic["Scenario"]["ActiveUnits"][key])):
+                try:
+                    if self.scen_dic["Scenario"]["ActiveUnits"][key][i]["Side"] == side_id_str:
+                        ar[key].append(self.scen_dic["Scenario"]["ActiveUnits"][key][i])
+                except KeyError:
+                    if self.scen_dic["Scenario"]["ActiveUnits"][key]["Side"] == side_id_str:
+                        ar[key].append(self.scen_dic["Scenario"]["ActiveUnits"][key])
         return ar
-
-    def get_side_doctrine(self, side_id = 0):
-        """Get the doctrine of a side"""
-        ar = {}
-        for i in range(len(self.root[19][side_id][5])):
-            try:
-                ar[self.root[19][side_id][5][i].tag + ": " + self.root[19][side_id][5][i].text] = i
-            except TypeError:
-                ar[self.root[19][side_id][5][i].tag + ": "] = i
-        return ar 
 
 if __name__ == '__main__':
     features = features_from_game_info("C:\\Users\\AFSOC A8XW ORSA\\Documents\\Python Proj\\AI\\pycmo\\raw\\wooden_leg.xml", "Israel")
--- pycmo/run_loop.py
+++ pycmo/run_loop.py
@@ -1,5 +1,6 @@
 """A run loop for agent/environment interaction."""
 
+from lib.features import Features
 from lib.protocol import Client, Server
 import threading
 import time
@@ -8,7 +9,11 @@ import json
 f = open("C:\\Users\\AFSOC A8XW ORSA\\Documents\\Python Proj\\AI\\pycmo\\pycmo\\configs\\config.json")
 config = json.load(f)
 
-server = Server("C:\\ProgramData\\Command Professional Edition 2\\Scenarios\\Standalone Scenarios\\Battle of Chumonchin Chan, 1950.scen")
+scen_file = "C:\\ProgramData\\Command Professional Edition 2\\Scenarios\\Standalone Scenarios\\Wooden Leg, 1985.scen"
+xml_file = "C:\\Users\\AFSOC A8XW ORSA\\Documents\\Python Proj\\AI\\pycmo\\raw\\wooden_leg.xml"
+player_side = "Israel"
+
+server = Server(scen_file)
 x = threading.Thread(target=server.start_game)
 x.start()
 
@@ -16,6 +21,29 @@ time.sleep(15)
 
 client = Client()
 client.connect()
-for i in range(5):
+""" for i in range(5):
     #client.get_raw_data(config["observation_path"] + str(i) + ".xml")
-    client.step_and_get_obs("01", "00", "00", config["observation_path"] + str(i) + ".xml")
+    client.step_and_get_obs("01", "00", "00", config["observation_path"] + str(i) + ".xml") """
+
+scen_end = False
+
+while not scen_end:
+    # add randomness to agent's actions
+    # agent.epsilon = 80 - counter_games
+
+    # get old state
+    state_old = Features(xml_file, player_side)
+
+    # perform random actions or choose the action
+    # final_move = 
+    # player1.do_move(final_move)
+
+    # get new state and observation, rewards, discount
+    # client.step_and_get_obs("01", "00", "00", xml_file)
+    # env.TimeStep('step_type', 'reward', 'discount', 'observation')
+    # new_state = Features(xml_file, player_side)
+
+    # store new data into a long-term memory
+
+    # reset loop
+    scen_end = True
