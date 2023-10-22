ecd458b0ffb230edeff076065f4066f314aca2cf
Minh <minhhua12345@gmail.com>
2023-10-21 22:33:41 -0500

    remove unused function from pycmo_lib

--- scripts/pycmo_lib.lua
+++ scripts/pycmo_lib.lua
@@ -1,29 +1,3 @@
--- Convert a contacts table into a lua syntactically correct string
-function contacts_table_to_string(tbl)
-    local result = "{"
-    for k, v in pairs(tbl) do
-        -- Check the key type (ignore any numerical keys - assume its an array)
-        if type(k) == "string" then
-            result = result.."[\""..k.."\"]".."="
-        end
-
-        -- Check the value type
-        if type(v) == "table" then
-            result = result..table_to_string(v)
-        elseif type(v) == "boolean" then
-            result = result..tostring(v)
-        else
-            result = result.."\""..v.guid.."\""
-        end
-        result = result..","
-    end
-    -- Remove leading commas from the result
-    if result ~= "{" then
-        result = result:sub(1, result:len()-1)
-    end
-    return result.."}"
-end
-
 function move_unit_to(side, unit_name, latitude, longitude)
     if latitude >= -90 and latitude <= 90 and longitude >= -180 and longitude <= 180 then
         ScenEdit_SetUnit({side = side, unitname = unit_name, course = {{longitude = longitude, latitude = latitude, TypeOf = 'ManualPlottedCourseWaypoint'}}})
