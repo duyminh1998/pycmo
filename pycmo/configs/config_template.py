1d6501688cf459cca43a93fcca9f6e9165af2094
Minh <minhhua12345@gmail.com>
2023-10-23 20:36:55 -0500

    remove config secrets

---
+++ pycmo/configs/config_template.py
@@ -0,0 +1,12 @@
+# Author: Minh Hua
+# Date: 08/22/2021
+# Purpose: Edit the paths in the get_config function as needed and rename this file to config.py.
+
+def get_config():
+    return {
+    "command_path": "path_to\\Command - Modern Operations\\",
+    "observation_path": "path_to\\\\pycmo\\\\raw\\\\steps\\\\",
+    "scen_ended": "path_to\\\\pycmo\\\\pycmo\\\\configs\\\\scen_has_ended.txt",
+    "command_cli_output_path": "C:\\ProgramData\\Command Professional Edition 2\\Analysis_Int", # only applicable to Premium version so we update this later
+    "pickle_path": "path_to\\pycmo\\pickle\\"
+    }
