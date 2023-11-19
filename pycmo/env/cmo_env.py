ddb9e793ccb07295dc2527ab606987340c280698
Minh <minhhua12345@gmail.com>
2023-11-19 00:27:40 -0600

    temp fix

--- pycmo/env/cmo_env.py
+++ pycmo/env/cmo_env.py
@@ -232,7 +232,8 @@ class CMOEnv():
                  observation_path: str, 
                  action_path: str,
                  scen_ended_path: str,
-                 pycmo_lua_lib_path: str = None):
+                 pycmo_lua_lib_path: str = None,
+                 max_resets: int = 10):
         self.client = SteamClient(scenario_name=scenario_name, agent_action_filename=action_path, command_version=command_version) # initialize a client to send data to the game
         if not self.client.connect(): # connect the client to the game
             raise FileNotFoundError("No running instance of Command to connect to.")
@@ -246,6 +247,7 @@ class CMOEnv():
             config = get_config()
             pycmo_lua_lib_path = os.path.join(config['pycmo_path'], 'lua', 'pycmo_lib.lua')
         self.pycmo_lua_lib_path = pycmo_lua_lib_path # the path to the pycmo_lib.lua file
+        self.max_resets = max_resets
 
         self.current_observation = None
         self.step_id = 0
@@ -254,6 +256,16 @@ class CMOEnv():
         try:
             if not self.client.restart_scenario():
                 raise ValueError("Client was not able to restart the scenario.")
+            
+            # check that the scenario loaded event has fired correctly in CMO, and if not, restart the scenario
+            retries = 0
+            while self.check_game_ended() and retries < self.max_resets:
+                print(f"Scenario not loaded properly. Retrying... (Attempt {retries} of {self.max_resets})")
+                if not self.client.restart_scenario():
+                    raise ValueError("Client was not able to restart the scenario.")
+                retries += 1
+            if self.check_game_ended():
+                raise ValueError("Scenario not restarting and loading properly. Please check game files.")
 
             self.client.send('')
 
