8367aadef14479b48b33fe557baa3a56b5bbd942
Minh <minhhua12345@gmail.com>
2023-11-18 19:48:20 -0600

    simplifying cmo_env params

--- scripts/floridistan/restart_demo.py
+++ scripts/floridistan/restart_demo.py
@@ -16,24 +16,20 @@ config = get_config()
 # MAIN LOOP
 scenario_name = "floridistan"
 player_side = "BLUE"
-step_size = ['0', '0', '1']
 scenario_script_folder_name = "floridistan" # name of folder containing the lua script that the agent will use
 
 command_version = config["command_mo_version"]
 observation_path = os.path.join(config['steam_observation_folder_path'], f'{scenario_name}.inst')
 action_path = os.path.join(config["scripts_path"], scenario_script_folder_name, "agent_action.lua")
 scen_ended_path = os.path.join(config['steam_observation_folder_path'], f'{scenario_name}_scen_has_ended.inst')
-pycmo_lua_lib_path = os.path.join(config['pycmo_path'], 'lua', 'pycmo_lib.lua')
 
 env = CMOEnv(
         scenario_name=scenario_name,
         player_side=player_side,
-        step_size=step_size,
         command_version=command_version,
         observation_path=observation_path,
         action_path=action_path,
         scen_ended_path=scen_ended_path,
-        pycmo_lua_lib_path=pycmo_lua_lib_path
 )
 
 attacker_name = "Thunder #1"
