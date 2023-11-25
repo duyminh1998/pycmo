654790cf1ba29ebea5c1c91a6e0792f4862f490d
Minh <minhhua12345@gmail.com>
2023-11-24 19:57:45 -0600

    add AvailableFunctionsSteam

--- pycmo/lib/actions.py
+++ pycmo/lib/actions.py
@@ -5,6 +5,7 @@
 
 # imports
 import collections
+from typing import Tuple
 
 from pycmo.lib.features import Features, FeaturesFromSteam
 
@@ -35,8 +36,86 @@ def rtb(side:str, unit_name:str) -> str:
 
 Function = collections.namedtuple("Function", ['id', 'name', 'corresponding_def', 'args', 'arg_types'])
 
-class AvailableFunctions():
+class AvailableFunctionsSteam():
   def __init__(self, features:Features | FeaturesFromSteam):
+    self.sides = [features.player_side]
+
+    self.unit_ids = self.get_unit_ids(features)
+    self.contact_ids = self.get_contact_ids(features)
+    self.mount_ids, self.loadout_ids, self.weapon_ids, self.weapon_qtys = self.get_weapons(features)
+
+    self.VALID_FUNCTIONS = self.get_valid_functions()
+
+  def get_unit_ids(self, features:Features|FeaturesFromSteam) -> list[str]:
+    return [unit.ID for unit in features.units]
+
+  def get_contact_ids(self, features:Features|FeaturesFromSteam) -> list[str]:
+    return [contact.ID for contact in features.contacts]
+  
+  def get_weapons(self, features:Features|FeaturesFromSteam) -> Tuple[dict, dict, dict, dict]:
+    mount_ids = {}
+    loadout_ids = {}
+    weapon_ids = {}
+    weapon_qtys = {}
+    for unit in features.units:
+      loadout_weapons = []
+      if unit.Mounts:
+        unit_mounts = []
+        for mount in unit.Mounts:
+          mount_weapons = []
+          unit_mounts.append(mount.DBID)
+          for weapon in mount.Weapons:
+            weapon_qtys[f"{unit.ID},{mount.DBID},{weapon.WeaponID}"] = weapon.QuantRemaining
+            mount_weapons.append(weapon.WeaponID)
+          weapon_ids[f"{unit.ID},{mount.DBID}"] = mount_weapons
+        mount_ids[unit.ID] = unit_mounts
+      if unit.Loadout:
+        loadout_ids[unit.ID] = [unit.Loadout.DBID]
+        for weapon in unit.Loadout.Weapons:
+          weapon_qtys[f"{unit.ID},{unit.Loadout.DBID},{weapon.WeaponID}"] = weapon.QuantRemaining
+          loadout_weapons.append(weapon.WeaponID)
+        weapon_ids[f"{unit.ID},{unit.Loadout.DBID}"] = loadout_weapons
+    return mount_ids, loadout_ids, weapon_ids, weapon_qtys
+  
+  def get_valid_functions(self) -> list[Function]:
+    mount_id_args = set([mount_id for unit_mount_ids in self.mount_ids.values() for mount_id in unit_mount_ids])
+    # loadout_id_args = set([loadout_id for unit_loadout_ids in self.loadout_ids.values() for loadout_id in unit_loadout_ids])
+    weapon_id_args = set([weapon_id for unit_weapon_ids in self.weapon_ids.values() for weapon_id in unit_weapon_ids])
+    weapon_qty_args = set([weapon_qty for weapon_qty in self.weapon_qtys.values()])
+    VALID_FUNCTION_ARGS = {
+      'no_op': [],
+      'launch_aircraft': [self.sides, self.unit_ids, ["true", "false"]],
+      'set_unit_course': [self.sides, self.unit_ids, [-90, 90], [-180, 180]],
+      'manual_attack_contact': [self.unit_ids, self.contact_ids, weapon_id_args, weapon_qty_args, mount_id_args],
+      'auto_attack_contact': [self.unit_ids, self.contact_ids],
+      'refuel_unit': [self.sides, self.unit_ids, self.unit_ids],
+      'rtb': [self.sides, self.unit_ids],
+      'auto_refuel_unit': [self.sides, self.unit_ids]
+    }
+    ARG_TYPES = {
+      'no_op': ['NoneChoice'],
+      'launch_aircraft': ['EnumChoice', 'EnumChoice', 'EnumChoice'],
+      'set_unit_course': ['EnumChoice', 'EnumChoice', 'Range', 'Range'],
+      'manual_attack_contact': ['EnumChoice', 'EnumChoice', 'EnumChoice', 'EnumChoice', 'EnumChoice'],
+      'auto_attack_contact': ['EnumChoice', 'EnumChoice'],
+      'refuel_unit': ['EnumChoice', 'EnumChoice', 'EnumChoice'],
+      'rtb': ['EnumChoice', 'EnumChoice'],
+      'auto_refuel_unit': ['EnumChoice', 'EnumChoice']
+    }
+    valid_functions = [
+      Function(0, "no_op", no_op, VALID_FUNCTION_ARGS['no_op'], ARG_TYPES['no_op']),
+      Function(1, "launch_aircraft", launch_aircraft, VALID_FUNCTION_ARGS['launch_aircraft'], ARG_TYPES['launch_aircraft']),
+      Function(2, 'set_unit_course', set_unit_course, VALID_FUNCTION_ARGS['set_unit_course'], ARG_TYPES['set_unit_course']),
+      Function(3, "manual_attack_contact", manual_attack_contact, VALID_FUNCTION_ARGS['manual_attack_contact'], ARG_TYPES['manual_attack_contact']),
+      Function(4, "auto_attack_contact", auto_attack_contact, VALID_FUNCTION_ARGS['auto_attack_contact'], ARG_TYPES['auto_attack_contact']),
+      Function(5, 'refuel_unit', refuel_unit, VALID_FUNCTION_ARGS['refuel_unit'], ARG_TYPES['refuel_unit']),
+      Function(6, 'rtb', rtb, VALID_FUNCTION_ARGS['rtb'], ARG_TYPES['rtb']),
+      Function(7, 'auto_refuel_unit', auto_refuel_unit, VALID_FUNCTION_ARGS['auto_refuel_unit'], ARG_TYPES['auto_refuel_unit'])
+    ]
+    return valid_functions
+
+class AvailableFunctions():
+  def __init__(self, features):
     sides = [features.player_side]
     units = []
     contacts = []
--- tests/test_actions.py
+++ tests/test_actions.py
@@ -3,8 +3,12 @@ import os
 
 from pycmo.configs.config import get_config
 from pycmo.lib.actions import *
+from pycmo.lib.tools import cmo_steam_observation_file_to_xml
 
 config = get_config()
+
+observation_file_path = os.path.join(config['pycmo_path'], 'tests', "fixtures", 'test_steam_observation.inst')
+scenario_xml = cmo_steam_observation_file_to_xml(observation_file_path)
 
 side = "Israel"
 tanker_name = "Chevron #6"
@@ -46,3 +50,80 @@ def test_auto_refuel_unit():
 
 def test_rtb():
     assert rtb(side, aircraft_name) == f"ScenEdit_SetUnit({{side = '{side}', name = '{aircraft_name}', RTB = true}})"
+
+features = FeaturesFromSteam(xml=scenario_xml, player_side=side)
+avail_funcs = AvailableFunctionsSteam(features=features)
+unit_id = "05ba3413-d0cd-4a69-8513-2d7e55d28366"
+mount_id = "286"
+loadout_id = "18127"
+mount_weapon_id = "1918"
+loadout_weapon_id = "516"
+def test_available_functions():
+    assert isinstance(avail_funcs, AvailableFunctionsSteam)
+
+def test_available_functions_sides():
+    assert len(avail_funcs.sides) == 1
+    assert avail_funcs.sides[0] == side
+
+def test_available_functions_unit_ids():
+    assert len(avail_funcs.unit_ids) == 9
+    assert isinstance(avail_funcs.unit_ids[0], str)
+    assert avail_funcs.unit_ids[0] == "8e2750c4-6c86-46a4-8dfa-16507c7f71e3"
+
+def test_available_functions_contact_ids():
+    assert len(avail_funcs.contact_ids) == 8
+    assert isinstance(avail_funcs.contact_ids[0], str)
+    assert avail_funcs.contact_ids[0] == "0HXVM6-0HMUTDCKTG4AA"
+
+def test_available_functions_mount_ids():
+    assert len(avail_funcs.mount_ids[unit_id]) == 2
+    assert isinstance(avail_funcs.mount_ids[unit_id], list)
+    assert avail_funcs.mount_ids[unit_id][0] == mount_id
+
+def test_available_functions_loadout_ids():
+    assert len(avail_funcs.loadout_ids[unit_id]) == 1
+    assert isinstance(avail_funcs.loadout_ids[unit_id], list)
+    assert avail_funcs.loadout_ids[unit_id][0] == loadout_id
+
+def test_available_functions_mount_weapon_ids():
+    key = f"{unit_id},{mount_id}"
+    assert len(avail_funcs.weapon_ids) == 20
+    assert isinstance(avail_funcs.weapon_ids[key], list)
+    assert avail_funcs.weapon_ids[key][0] == mount_weapon_id
+
+def test_available_functions_loadout_weapon_ids():
+    key = f"{unit_id},{loadout_id}"
+    assert len(avail_funcs.weapon_ids) == 20
+    assert isinstance(avail_funcs.weapon_ids[key], list)
+    assert avail_funcs.weapon_ids[key][0] == loadout_weapon_id
+
+def test_available_functions_mount_weapon_qtys():
+    key = f"{unit_id},{mount_id},{mount_weapon_id}"
+    assert len(avail_funcs.weapon_qtys) == 45
+    assert isinstance(avail_funcs.weapon_qtys[key], int)
+    assert avail_funcs.weapon_qtys[key] == 5
+
+def test_available_functions_loadout_weapon_qtys():
+    key = f"{unit_id},{loadout_id},{loadout_weapon_id}"
+    assert len(avail_funcs.weapon_qtys) == 45
+    assert isinstance(avail_funcs.weapon_qtys[key], int)
+    assert avail_funcs.weapon_qtys[key] == 4
+
+def test_available_functions_valid_functions():
+    manual_attack_fnc = avail_funcs.VALID_FUNCTIONS[3]
+    assert isinstance(avail_funcs.VALID_FUNCTIONS, list)
+    assert len(avail_funcs.VALID_FUNCTIONS) == 8
+    assert isinstance(manual_attack_fnc, Function)
+    assert manual_attack_fnc.id == 3
+    assert manual_attack_fnc.name == "manual_attack_contact"
+    assert manual_attack_fnc.corresponding_def == manual_attack_contact
+    assert isinstance(manual_attack_fnc.args, list)
+    weapon_id_args = manual_attack_fnc.args[2]
+    assert isinstance(weapon_id_args, set)
+    assert isinstance(list(weapon_id_args)[0], str)
+    weapon_qty_args = manual_attack_fnc.args[3]
+    assert isinstance(weapon_qty_args, set)
+    assert isinstance(list(weapon_qty_args)[0], int)
+    mount_id_args = manual_attack_fnc.args[4]
+    assert isinstance(mount_id_args, set)
+    assert isinstance(list(mount_id_args)[0], str)
