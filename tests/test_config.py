ad8f326734096608000c197e389ebb3c1869a775
Minh <minhhua12345@gmail.com>
2023-12-11 20:05:42 -0600

    update config test for gymnasium variable

--- tests/test_config.py
+++ tests/test_config.py
@@ -7,5 +7,5 @@ config = get_config()
 
 @pytest.mark.parametrize("requested_path, expected", [(k, True) for k in config.keys()])
 def test_paths(requested_path, expected):
-    if requested_path != 'command_mo_version':
+    if requested_path != 'command_mo_version' and requested_path != "gymnasium":
         assert os.path.exists(config[requested_path]) == expected
